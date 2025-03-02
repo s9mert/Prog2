c2b68d17166a0a8e89540f794bcae77d5677ee4c
Vlada <vladlena.velenteenko@gmail.com>
2025-03-02 12:34:26 +0100

    MovieAPI intiallized

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/MovieAPI.java
@@ -0,0 +1,16 @@
+package at.ac.fhcampuswien.fhmdb;
+
+public class MovieAPI {
+
+   /* private static final String BASE_URL = "https://prog2.fh-campuswien.ac.at/movies";
+
+    public static list<Movie> getMovies (String query, String genre, String releaseYear, String ratingFrom){
+        HttpUrl.Builder urlBuilder = HttpUrl;
+
+    }
+
+    */
+}
+
+
+
